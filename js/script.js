cb7fb35e7d7b3b70ba13a0d253f292357f7f04af
MyhailoGo <myhailoustyak@ukr.net>
2021-03-06 00:45:46 +0200

    add new changes

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-let doc = document;
-
-let fight = doc.getElementById("fight"),
+const doc = document,
+fight = doc.getElementById("fight"),
 attacking = doc.querySelectorAll(".attack"),
 defending = doc.querySelectorAll(".defend"),
 greenHpPlayer = doc.querySelector(".playerHealthNow"),
 redHpPlayer = doc.querySelector(".playerHealthLost"),
 greenHpBot = doc.querySelector(".botHealthNow"),
 redHpBot = doc.querySelector(".botHealthLost"),
-log = doc.querySelector('.log'),
-nameUser = prompt("Введіть своє ім'я", "Магістр Йода"); 
+log = doc.querySelector('.log');
+
+let  nameUser = prompt("Введіть своє ім'я", "Магістр Йода");
 nameUser = nameUser === null || nameUser === "" ? "Магістр Йода" : nameUser.trim().split("").slice(0,15).join("");
 
 
-bot = {
+const bot = {
 	name: "Darth Vader",
 	health: 100,
-	attack: rand,
-	defend: rand,
+	attack: ()=>getRandomValue(1,3),
+	defend: ()=>getRandomValue(1,3),
 	greenHp: greenHpBot,
 	redHp: redHpBot,
 },
@@ -30,16 +30,11 @@ player = {
 };
 doc.querySelector(".playerName").innerHTML = nameUser;
 
-function rand(){
-return Math.floor(1 + Math.random() * 3);
+function getRandomValue(min, max) {
+  	return Math.floor(min + Math.random() * (max + 1 - min));
 }
 
-function random(min, max) {
-	let rand = min + Math.random() * (max + 1 - min);
-  	return Math.floor(rand);
-}
-
-function boy(health,object){
+function showHealth(health,object){
 	if(health<=0){
 		object.greenHp.style.width = "0px";
 		object.redHp.style.width = "250px";
@@ -56,17 +51,16 @@ function boy(health,object){
 function showMsg(){
 
 }
-let a;
-let b;
+let showMessageLog1;
+let showMessageInput;
 
 function playerAttacking(){
 	for(let i=0; i<attacking.length; i++){
 		if(attacking[i].checked){
-			let botDefended;
-			botDefended = bot.defend();
+			let botDefended = bot.defend();
 			
 			if(+attacking[i].value !== +botDefended){
-				let damage = random(5,10)
+				let damage = getRandomValue(5,10)
 				let msgDamageLog;
 					switch(+attacking[i].value){
 						case 1:
@@ -81,10 +75,10 @@ function playerAttacking(){
 					}
 				
 				console.log(`${player.name} наносить боту ${bot.name} ${damage} урону ${msgDamageLog}`)
-					 b = `${player.name} наносить  ${bot.name} ${damage} урону ${msgDamageLog}`
+					 showMessageInput = `${player.name} наносить  ${bot.name} ${damage} урону ${msgDamageLog}`
 					 bot.health -= damage;
 					 bot.health <= 0 ? 0 : bot.health;
-					 boy(bot.health, bot);
+					 showHealth(bot.health, bot);
 					 break;
 			}
 					else switch(+botDefended){
@@ -100,7 +94,7 @@ function playerAttacking(){
 					}
 
 						console.log(`${bot.name} заблокував удар ${msgDamageLog}`)
-						b = `${bot.name} заблокував удар ${msgDamageLog}`;
+						showMessageInput = `${bot.name} заблокував удар ${msgDamageLog}`;
 					
 		}
 	}
@@ -110,11 +104,11 @@ function playerAttacking(){
 function playerDefending(){
 	for(let i=0; i<defending.length; i++){
 		if(defending[i].checked){
-			let botAttacked;
-				botAttacked = bot.attack();
+			let botAttacked = bot.attack();;
+				
 			let msgDamageLog;
 				if (+defending[i].value !== +botAttacked){
-					 damage = random(5,10);
+					 damage = getRandomValue(5,10);
 						
 					switch(+botAttacked){
 						case 1:
@@ -129,10 +123,10 @@ function playerDefending(){
 					}
 				
 					console.log(`${bot.name} наносить ${player.name} ${damage} урону ${msgDamageLog}`);
-					 a = `${bot.name} наносить ${player.name} ${damage} урону ${msgDamageLog}`
+					 showMessageLog1 = `${bot.name} наносить ${player.name} ${damage} урону ${msgDamageLog}`
 						player.health -= damage;
 						player.health <= 0 ? 0 : player.health;
-						boy(player.health, player);
+						showHealth(player.health, player);
 							break;
 				}
 						
@@ -151,42 +145,48 @@ function playerDefending(){
 			}
 
 				console.log(`${player.name} заблокував удар ${msgDamageLog} `)
-				a = `${player.name} заблокував удар ${msgDamageLog} `;
+				showMessageLog1 = `${player.name} заблокував удар ${msgDamageLog} `;
 		}
 	}
 }
 /* Захист гравця */
 
 
-function addDiv (a,b){
+function addDiv (showMessageA,showMessageB){
 	let div = doc.createElement("div");
 	div.classList.add("fightLog");
 	div.insertAdjacentHTML(`afterbegin`,`
-				${b}<br>
- 				${a} <br>
- 			Hp ${player.name}	${player.health} ${bot.health} Darth Vader Hp`)
-	document.querySelector(".log").prepend(div)
+				${showMessageB}<br>
+ 				${showMessageA} <br>
+ 			<span class = "playersHp"> Hp ${player.name}	${player.health}</span><span class ="botsHp"> ${bot.health} Darth Vader Hp </span>`);
+	document.querySelector(".log").prepend(div);
+};
 
+function questionPlayAgain(){
+	confirm("Play again?") ? location.reload() : alert("Good luck");
 }
 
 function gameOver(player, bot){
 	if(player.health <= 0 && bot.health <= 0){
-		alert("Draw")
+		alert("Draw");
+		questionPlayAgain();
 	}
 	else if (player.health<=0){
 		alert(bot.name + " winner!")
+		questionPlayAgain();
 	}
 	else if(bot.health<=0){
 		alert(`Congratulation!!!! ${player.name}  winner!!` )
+		questionPlayAgain();
 	}
-}
+};
 
 
 
 fight.onclick = function(){
-	playerAttacking()
-	playerDefending()
-	addDiv(a,b)
-	gameOver(player,bot)
+	playerAttacking();
+	playerDefending();
+	addDiv(showMessageLog1,showMessageInput);
+	gameOver(player,bot);
 	console.log(player.health, bot.health)
-}
+};
